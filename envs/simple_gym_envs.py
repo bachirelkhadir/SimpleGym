e120e6756c5f3cd706ec42ac169666df1596d21d
bachirelkhadir <bachir009+git@gmail.com>
2021-04-21 13:40:03 -0400

    .

--- envs/simple_gym_envs.py
+++ envs/simple_gym_envs.py
@@ -3,9 +3,33 @@ from gym import spaces
 from gym.utils import seeding
 import numpy as np
 
+class SimpleEnv:
 
-class OneA_ZeroO_OneT_OneR(gym.Env):
-    """ One action, zero observation, one timestep long, +1 reward every timestep: This isolates the value network. If my agent can't learn that the value of the only observation it ever sees it 1, there's a problem with the value loss calculation or the optimizer."""
+    def correct_value_fn(self, s):
+        return 0
+
+    def error_value_fn(self, v):
+        """
+        How far is v from the true baseline?
+        """
+        return np.mean(np.abs([v(s) - self.correct_value_fn(s) for s in
+                        [self.observation_space.sample() for _ in range(1000)]]))
+
+
+    def error_policy(self, pi):
+        """
+        How far is v from the true baseline?
+        """
+        return 0
+
+
+class OneA_ZeroO_OneT_OneR(gym.Env, SimpleEnv):
+    """One action, zero observation, one timestep long, +1 reward every timestep:
+    This isolates the value network. If my agent can't learn that the value of the
+    only observation it ever sees it 1, there's a problem with the value loss
+    calculation or the optimizer.
+    """
+
     def __init__(self):
         super().__init__()
         self.observation_space = spaces.Discrete(1)
@@ -21,9 +45,16 @@ class OneA_ZeroO_OneT_OneR(gym.Env):
         o, r, d, info = self.state, self.reward, True, {}
         return o, r, d, info
 
+    def correct_value_fn(self, s):
+        return 1.
 
-class OneA_RandomO_OneT_PmR(gym.Env):
-    """*One action, random +1/-1 observation, one timestep long, obs-dependent +1/-1 reward every time*: If my agent can learn the value in (1.) but not this one - meaning it can learn a constant reward but not a predictable one! - it must be that backpropagation through my network is broken."""
+
+
+class OneA_RandomO_OneT_PmR(gym.Env, SimpleEnv):
+    """*One action, random +1/-1 observation, one timestep long, obs-dependent
++1/-1 reward every time*: If my agent can learn the value in (1.) but not this
+one - meaning it can learn a constant reward but not a predictable one! - it
+must be that backpropagation through my network is broken."""
 
     def __init__(self):
         super().__init__()
@@ -37,12 +68,20 @@ class OneA_RandomO_OneT_PmR(gym.Env):
         return self.state
 
     def step(self, a):
-        self.state = self.observation_space.sample()
         r = 1 if self.state == 0 else -1
+        self.state = self.observation_space.sample()
         return self.state, r, True, {}
 
-class OneA_ZeroThenOneO_TwoT_OneEndR(gym.Env):
-    """One action, zero-then-one observation, two timesteps long, +1 reward at the end*: If my agent can learn the value in (2.) but not this one, it must be that my reward discounting is broken."""
+    def correct_value_fn(self, s):
+        return [1., -1][s]
+
+
+class OneA_ZeroThenOneO_TwoT_OneEndR(gym.Env, SimpleEnv):
+    """One action, zero-then-one observation, two timesteps long, +1 reward at
+    the end*: If my agent can learn the value in (2.) but not this one, it must
+    be that my reward discounting is broken.
+
+    S: 0 -> 1 -> 2 R: 0 -> 1 -> 0 """
 
     def __init__(self):
         super().__init__()
@@ -56,13 +95,16 @@ class OneA_ZeroThenOneO_TwoT_OneEndR(gym.Env):
         return self.state
 
     def step(self, a):
+        r = 1 if self.state == 1 else 0
         self.state = self.state + 1
-        r = 1 if self.state == 2 else 0
         d = self.state == 2
         return self.state, r, d, {}
 
+    def correct_value_fn(self, s):
+        return 1. if s == 1 else 0.
 
-class TwoA_ZeroO_OneT_ActionDependentR(gym.Env):
+
+class TwoA_ZeroO_OneT_ActionDependentR(gym.Env, SimpleEnv):
 
     """Two actions, zero observation, one timestep long, action-dependent +1/-1 reward*: The first env to exercise the policy! If my agent can't learn to pick the better action, there's something wrong with either my advantage calculations, my policy loss or my policy update. That's three things, but it's easy to work out by hand the expected values for each one and check that the values produced by your actual code line up with them."""
 
@@ -81,7 +123,15 @@ class TwoA_ZeroO_OneT_ActionDependentR(gym.Env):
         d = True
         return self.state, r, d, {}
 
-class TwoA_RandomO_OneT_ActionDependentR(gym.Env):
+    def correct_value_fn(self, s):
+        return 1.
+
+    def error_policy(self, pi):
+        return np.mean(np.abs([pi(s) - 0. for s in
+                        [self.observation_space.sample() for _ in range(1000)]]))
+
+
+class TwoA_RandomO_OneT_ActionDependentR(gym.Env, SimpleEnv):
 
     """
     Two actions, random +1/-1 observation, one timestep long, action-and-obs dependent +1/-1 reward:
@@ -102,3 +152,54 @@ class TwoA_RandomO_OneT_ActionDependentR(gym.Env):
         r = 1 if a == self.state else -1
         d = True
         return self.state, r, d, {}
+
+    def correct_value_fn(self, s):
+        return 1.
+
+
+    def error_policy(self, pi):
+        # optimal action = state
+        return np.mean(np.abs([pi(s) - s for s in
+                        [self.observation_space.sample() for _ in range(1000)]]))
+
+
+# Continuous Envs
+class C_OneA_ZeroO_OneT_ActionDependentR(gym.Env, SimpleEnv):
+    """One action a in (-1, 1), zero observation, one timestep long,
+    action-dependent reward r = f(a) f = 2*exp(-(x-0.5)**2/.1)+
+    exp(-(x+0.5)**2/.1)
+
+
+    Optimal solution
+    pi(s) = 1/2
+    V(s) = 2
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.observation_space = spaces.Box(-1, 1, shape=(1,))
+        self.action_space = spaces.Box(-1, 1, shape=(1,))
+        self.reward_fn = lambda x: 2*np.exp(-(x-0.5)**2/.1)+ np.exp(-(x+0.5)**2/.1)
+        self.best_action = 0.5 # maximizer
+
+    def reset(self):
+        self.state = self.observation_space.sample()
+        return self.state
+
+    def step(self, a):
+        r = self.reward_fn(a)
+        d = True
+        return self.state, r, d, {}
+
+    def correct_value_fn(self, s):
+        return self.reward_fn(self.best_action)
+
+    def error_policy(self, pi):
+
+        return np.mean(np.abs([pi(s) - 0.5 for s in
+                        [self.observation_space.sample() for _ in range(1000)]]))
+
+# Local Variables:
+# fill-column: 80
+# End:
--- envs/simple_gym_envs_test.py
+++ envs/simple_gym_envs_test.py
@@ -7,16 +7,22 @@ ENVS = [OneA_ZeroO_OneT_OneR,
         OneA_RandomO_OneT_PmR,
         OneA_ZeroThenOneO_TwoT_OneEndR,
         TwoA_ZeroO_OneT_ActionDependentR,
-        TwoA_RandomO_OneT_ActionDependentR,]
+        TwoA_RandomO_OneT_ActionDependentR,
+        C_OneA_ZeroO_OneT_ActionDependentR]
+
 
 for env_name in ENVS:
     print("=== Test: {} ===".format(env_name))
 
 
     env = env_name() #gym.make(env_name)
+    dummy_policy = lambda _: env.observation_space.sample()
+    dummy_baseline = lambda _: np.random.randn()
     env.random_start = False
     print("Obs:", env.observation_space)
     print("Action:", env.action_space)
+    print("Error/policy:", env.error_policy(dummy_policy))
+    print("Error/baseline:", env.error_policy(dummy_baseline))
 
     env.reset()
 
